b210094eea9f9e66f9f9d1d2cea947f6a510c15a
Mustafa Khan <PunkUnknown@tutanota.com>
2023-04-11 23:07:39 +0500

    Initial docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Argentite</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/orgs/Argentite-org/repositories",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+};
 
-export default config
+export default config;
